cd47e0af996bf8725b777b50c1353b60888c3c0d
ArnoDorian <abhinavrawat882@gmail.com>
2026-01-14 15:17:28 +0530

    Added to-do-list Project

--- Core.API/Program.cs
+++ Core.API/Program.cs
@@ -49,24 +49,6 @@ app.UseSwaggerUI();
 // This must be placed AFTER UseHttpsRedirection and BEFORE MapControllers
 app.UseCors("AllowAll");
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.MapControllers();
 
